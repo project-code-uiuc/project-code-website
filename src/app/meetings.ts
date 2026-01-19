a032a8460c36924654337be0cd62d38ab4b48225
gjburke2 <gjburke2@illinois.edu>
2026-01-19 13:47:47 -0600

    Updated meetings

--- src/app/meetings.ts
+++ src/app/meetings.ts
@@ -9,50 +9,41 @@ export interface Meeting {
 export const MEETINGS: Meeting[] = [
   {
     title: "Fall General Meeting #1 - Info Session",
-    date: "Wed Sep 3, 2025, 6-7 PM",
-    location: "Siebel 1404",
-    locationLink: "https://maps.app.goo.gl/yCpsPaRFWMEaEnWt6",
+    date: "Wed Jan 28, 2025, 6-7 PM",
+    location: "Everitt Lab 1306",
+    locationLink: "https://maps.app.goo.gl/nr9NecfCWTPTd1HK8",
     description:
       "Learn more about Project: Code and how to get started on a project this semester!",
   },
   {
-    title: "Fast Enterprises - Resume Workshop",
-    date: "Tue Sep 9, 2025, 8-9 PM",
-    location: "DCL 1310",
-    locationLink: "https://maps.app.goo.gl/iLi2grocoZwMHrKF7",
-    description:
-      "Learn about creating a powerful resume with Fast Enterprises, as well as more about Project Code!",
-  },
-  {
-    title: "Fall General Meeting #2 - Projects Intro",
+    title: "Fall General Meeting #2 - Project Presentations",
     date: "Wed Sep 17, 6-7 PM",
-    location: "Loomis 151",
-    locationLink: "https://maps.app.goo.gl/ezuk4uzRfioekwAA8",
+    location: "Siebel 1404",
+    locationLink: "https://maps.app.goo.gl/ZbpULkaStfVezV52A",
     description:
       "Learn about the projects being held this semester and select the project you are most interested in working on!",
   },
-  {
-    title: "Fall General Meeting #3 - Midpoint Presentations",
-    date: "Wed Oct 29, 2025, 6-7 PM",
-    location: "Siebel 1404",
-    locationLink: "https://maps.app.goo.gl/yCpsPaRFWMEaEnWt6",
-    description:
-      "Hear from project groups about the progress they've made so far this semester!",
-  },
-
-  {
-    title: "Fall General Meeting #4 - Final Presentations",
-    date: "Thu Dec 4, 2025, 6-7 PM",
-    location: "Loomis 144",
-    locationLink: "https://maps.app.goo.gl/ezuk4uzRfioekwAA8",
-    description:
-      "Hear from project groups again and see each team's final product!",
-  },
+  // {
+  //   title: "Fall General Meeting #3 - Midpoint Presentations",
+  //   date: "Wed Oct 29, 2025, 6-7 PM",
+  //   location: "Siebel 1404",
+  //   locationLink: "https://maps.app.goo.gl/yCpsPaRFWMEaEnWt6",
+  //   description:
+  //     "Hear from project groups about the progress they've made so far this semester!",
+  // },
 
   // {
-  //   title: "More meetings coming soon!",
-  //   // date: "Wed, Jan 24, 2024, 6–7 PM",
+  //   title: "Fall General Meeting #4 - Final Presentations",
+  //   date: "Thu Dec 4, 2025, 6-7 PM",
+  //   location: "Loomis 144",
+  //   locationLink: "https://maps.app.goo.gl/ezuk4uzRfioekwAA8",
   //   description:
-  //     "Keep an eye on Discord and this site so you never miss a meeting! You can also subscribe to our calendar with the links above.",
+  //     "Hear from project groups again and see each team's final product!",
   // },
+  {
+    title: "More meetings coming soon!",
+    // date: "Wed, Jan 24, 2024, 6–7 PM",
+    description:
+      "Keep an eye on Discord and this site so you never miss a meeting! You can also subscribe to our calendar with the links above.",
+  },
 ];
